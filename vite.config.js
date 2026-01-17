490a13eabbd7c0a40f9b2e70832834bdffecfede
Spoorthy Bhat <spoorthybhat@Spoorthys-MacBook-Air.local>
2026-01-17 15:38:43 -0800

    Fix: Use correct base path for dev vs build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/100-game/' : '/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/100-game/' : '/',
+  }
 })
